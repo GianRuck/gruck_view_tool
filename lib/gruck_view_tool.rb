96b1f24eb9c813c4d0f3dcdec2de9e2aa728e980
Cottonwood Devcamp <cottonwood@devcamp.com>
2017-07-25 16:10:16 +0000

    Dev tool initial commit

---
+++ lib/gruck_view_tool.rb
@@ -0,0 +1,6 @@
+require "gruck_view_tool/version"
+require "gruck_view_tool/renderer"
+
+module GruckViewTool
+  # Your code goes here...
+end
---
+++ lib/gruck_view_tool/version.rb
@@ -0,0 +1,3 @@
+module GruckViewTool
+  VERSION = "0.1.0"
+end
